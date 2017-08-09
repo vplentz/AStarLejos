0f15c76d3ad0b6920408ee7708c5cde90727085f
Vítor Resing Plentz <vplentz_9@hotmail.com>
2017-08-09 14:46:50 -0300

    Update Main

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ import lejos.nxt.Button;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		AStarr astar = new AStarr();
-		astar.process();
+		AStarr astar = new AStarr(new MyPair(2,0), new MyPair(0,0), 1);
 		Button.ENTER.waitForPressAndRelease();
 	}
 }
